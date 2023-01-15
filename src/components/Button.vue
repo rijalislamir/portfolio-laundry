4947425aed0597f7c01f72ce1a485c09690de2a0
rijalislamir <rijalislamir@gmail.com>
2023-01-15 20:00:03 +0700

    add simple navbar

---
+++ src/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+button {
+  background-color: #0078d7;
+  border: none;
+  border-radius: 25px;
+  padding: 8px 24px;
+  color: white;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,52 @@
+<template>
+  <nav>
+    <div class="nav-container">
+      <h1>Logo</h1>
+
+      <div class="right-nav">
+        <ul>
+          <li>Pricing</li>
+          <li>Features</li>
+        </ul>
+
+        <span>|</span>
+
+        <Button>CONTACT US</Button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, .2);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 4rem;
+}
+
+.nav-container {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 1000px;
+}
+
+.right-nav {
+  display: flex;
+  line-height: 36px;
+  gap: 24px;
+  text-align: center;
+}
+
+ul {
+  display: flex;
+  gap: 24px;
+  list-style: none;
+}
+</style>
+
+<script setup lang="ts">
+import Button from './Button.vue';
+</script>
