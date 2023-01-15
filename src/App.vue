30e38b0dac054c980d089a42d38d84e2b90cd578
rijalislamir <rijalislamir@gmail.com>
2023-01-15 20:59:13 +0700

    add simple footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Navbar from "./components/Navbar.vue";
 import Jumbotron from "./components/Jumbotron.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <Navbar />
   <Jumbotron />
+  <Footer />
 </template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    <div>© Rijal Islami</div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  background-color: rgb(16, 24, 32);
+  color: white;
+  display: flex;
+  justify-content: center;
+  padding: 36px;
+}
+
+div {
+  max-width: 1000px;
+  text-align: center;
+  width: 100%;
+}
+</style>
