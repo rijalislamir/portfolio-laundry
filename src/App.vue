ceb1d2ad881974ccbb5ea1d6781789de154b9198
rijalislamir <rijalislamir@gmail.com>
2023-01-15 20:32:32 +0700

    add jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import Navbar from "./components/Navbar.vue";
+import Jumbotron from "./components/Jumbotron.vue";
 </script>
 
 <template>
   <Navbar />
+  <Jumbotron />
 </template>
---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Hello World!</h1>
+  </div>
+</template>
+
+<style scoped>
+div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+h1 {
+  display: inline;
+  background: linear-gradient(120deg, #00DBDE, #FC00FF);
+  background-clip: text;
+  font-size: 72px;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+</style>
