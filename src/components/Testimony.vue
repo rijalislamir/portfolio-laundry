217620ce8385793e3f94803d95bc9b93e59982a2
rijalislamir <rijalislamir@gmail.com>
2023-01-27 19:31:29 +0700

    remove navigation button on testimonial

--- src/components/Testimony.vue
+++ src/components/Testimony.vue
@@ -2,54 +2,53 @@
   <section class="bg-gray-100 py-4">
     <h1 class="text-center text-3xl font-bold px-6">What people say</h1>
 
-    <Swiper
-      :modules="modules"
-      :slides-per-view="1"
-      :space-between="10"
-      navigation
-      :pagination="{
-        clickable: true,
-        dynamicBullets: true,
-        dynamicMainBullets: 2,
-      }"
-    >
-      <SwiperSlide v-for="person in data">
-        <div class="text-center p-8 m-4 bg-gray-200 rounded-3xl h-full">
+    <div class="max-w-[1000px] mx-auto">
+      <Swiper
+        :modules="modules"
+        :slides-per-view="screenWidth < 1000 ? 1 : 2"
+        :space-between="10"
+        :pagination="{ clickable: true }"
+      >
+        <SwiperSlide v-for="person in data" class="flex justify-center">
           <div
-            class="bg-slate-900 w-24 h-24 rounded-full m-auto flex items-center justify-center"
+            class="text-center p-8 m-4 max-w-[463px] bg-gray-200 rounded-3xl h-full"
           >
-            <img :src="userIcon" alt="user-photo" />
+            <div
+              class="bg-slate-900 w-24 h-24 rounded-full m-auto flex items-center justify-center"
+            >
+              <img :src="userIcon" alt="user-photo" />
+            </div>
+
+            <span class="font-bold text-lg inline-block p-4">
+              {{ person.name }}
+            </span>
+
+            <p class="italic">
+              {{ person.text }}
+            </p>
           </div>
-
-          <span class="font-bold text-lg inline-block p-4">
-            {{ person.name }}
-          </span>
-
-          <p class="italic">
-            {{ person.text }}
-          </p>
-        </div>
-      </SwiperSlide>
-    </Swiper>
+        </SwiperSlide>
+      </Swiper>
+    </div>
   </section>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, onMounted, onUnmounted } from "vue";
 import { Swiper, SwiperSlide } from "swiper/vue";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Pagination, Scrollbar, A11y } from "swiper";
 import userIcon from "../assets/img/icons/icons8-user-60.png";
 
 import "swiper/css";
-import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
-const modules = [Navigation, Pagination, Scrollbar, A11y];
+const screenWidth = ref(0);
+const modules = [Pagination, Scrollbar, A11y];
 const data = [
   {
     name: "John Doe",
-    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi qui reprehenderit beatae nesciunt harum tempore veniam, perspiciatis quisquam commodi, reiciendis voluptas. Quasi inventore aperiam repellendus.",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi qui reprehenderit beatae nesciunt harum tempore veniam, perspiciatis quisquam commodi.",
   },
   {
     name: "Jessica Lawrence",
@@ -64,4 +63,14 @@ const data = [
     text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque placeat velit commodi eius hic adipisci! Vitae quos suscipit unde asperiores.",
   },
 ];
+
+const onWidthChange = () => (screenWidth.value = window.innerWidth);
+
+onMounted(() => {
+  window.addEventListener("resize", onWidthChange);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("resize", onWidthChange);
+});
 </script>
