b29df6bb39440de6703defbfbee34b6c38c73b66
rijalislamir <rijalislamir@gmail.com>
2023-01-28 14:01:32 +0700

    rename jumbotron to hero

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import Navbar from "./components/Navbar.vue";
-import Jumbotron from "./components/Jumbotron.vue";
+import Hero from "./components/Hero.vue";
 import Testimony from "./components/Testimony.vue";
 import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <Navbar />
-  <Jumbotron />
+  <Hero />
   <Testimony />
   <Footer />
 </template>
